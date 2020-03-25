6289cbdd96f51c195ed6bb0ebade26bace2646f4
Arnaud Provoost <r0792836@student.thomasmore.be>
2020-03-25 13:15:45 +0100

    echo voor alles en form werkt

--- dist/inschrijving.php
+++ dist/inschrijving.php
@@ -4,5 +4,15 @@ if (isset($_POST["InputVoornaam"]) && $_POST["InputVoornaam"] != "") {
 } else {
     echo "<p> je hebt geen voornaam ingevuld</p>\n";
 }
+if (isset($_POST["InputAchternaam"]) && $_POST["InputAchternaam"] != "") {
+    echo "<p> Ingevuld bij naam: " . $_POST["InputAchternaam"] . "</p>\n";
+} else {
+    echo "<p> Je hebt geen achternaam ingevuld</p>\n";
+}
 
+if (isset($_POST["InputEmail1"]) && $_POST["InputEmail1"] != "") {
+    echo "<p> Ingevuld bij naam: " . $_POST["InputEmail1"] . "</p>\n";
+} else {
+    echo "<p> Je hebt geen e-mailadress ingevuld</p>\n";
+}
 ?>
